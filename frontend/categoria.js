7ac9b5e2b8a4749d1cccb9563d1bfe95733d5248
rattopio123 <lmortola@pioix.edu.ar>
2026-06-17 10:06:40 -0300

    ACTUALIZACIONES DE CSS
    Se agrego el css del menu y la seleccion de categoria, tamibien se corrigio la vinculacion  del juego.js.
    TAmbien se agrego codigo de categoria js

--- frontend/categoria.js
+++ frontend/categoria.js
@@ -1,9 +1,54 @@
+/*document
+    .querySelector("#btnComenzar")
+    .addEventListener("click", () => {
+
+        const categoria =
+            document.querySelector("#categoria").value;
+
+        localStorage.setItem(
+            "categoria",
+            categoria
+        );
+
+        window.location.href =
+            "juego.html";
+
+    });*/
+
+    document
+    .querySelectorAll(".card")
+    .forEach(card => {
+
+        card.addEventListener("click", () => {
+
+            document
+                .querySelectorAll(".card")
+                .forEach(c =>
+                    c.classList.remove("seleccionada")
+                );
+
+            card.classList.add("seleccionada");
+
+            document
+                .querySelector("#categoria")
+                .value =
+                    card.dataset.id;
+
+        });
+
+    });
+
 document
     .querySelector("#btnComenzar")
     .addEventListener("click", () => {
 
         const categoria =
             document.querySelector("#categoria").value;
+
+        if(categoria === ""){
+            alert("Seleccioná una categoría");
+            return;
+        }
 
         localStorage.setItem(
             "categoria",
